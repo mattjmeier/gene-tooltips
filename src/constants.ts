896112a747fe69096fbe8f457ea2bab751a9f36a
mattjmeier <matthew.james.meier@gmail.com>
2025-10-04 15:40:56 -0400

    fix speices ideograms

--- src/constants.ts
+++ src/constants.ts
@@ -35,14 +35,14 @@ interface SpeciesInfo {
 export const speciesMap: Record<number, SpeciesInfo> = {
   9606: { common: "Human", genus: "Homo sapiens", icon: "🧑", ideogramName: "human" },
   10090: { common: "Mouse", genus: "Mus musculus", icon: "🐭", ideogramName: "mouse" },
-  10116: { common: "Rat", genus: "Rattus norvegicus", icon: "🐀" },
-  7227: { common: "Fruitfly", genus: "Drosophila melanogaster", icon: "🪰" },
-  6239: { common: "Nematode", genus: "Caenorhabditis elegans", icon: "🪱" },
+  10116: { common: "Rat", genus: "Rattus norvegicus", icon: "🐀", ideogramName: "rattus-norvegicus" },
+  7227: { common: "Fruitfly", genus: "Drosophila melanogaster", icon: "🪰", ideogramName: "drosophila-melanogaster" },
+  6239: { common: "Nematode", genus: "Caenorhabditis elegans", icon: "🪱", ideogramName: "caenorhabditis-elegans" },
   7955: { common: "Zebrafish", genus: "Danio rerio", icon: "🐟", ideogramName: "zebrafish" },
-  3702: { common: "Thale-cress", genus: "Arabidopsis thaliana", icon: "🌱" },
-  8364: { common: "Frog", genus: "Xenopus tropicalis", icon: "🐸" },
-  9823: { common: "Pig", genus: "Sus scrofa", icon: "🐖" },
-  559292 : { common: "Yeast", genus: "Saccharomyces cerevisiae", icon: "🧫" },
+  3702: { common: "Thale-cress", genus: "Arabidopsis thaliana", icon: "🌱", ideogramName: "arabidopsis-thaliana" },
+  8364: { common: "Frog", genus: "Xenopus tropicalis", icon: "🐸", ideogramName: "xenopus-tropicalis" },
+  9823: { common: "Pig", genus: "Sus scrofa", icon: "🐖", ideogramName: "sus-scrofa" },
+  559292 : { common: "Yeast", genus: "Saccharomyces cerevisiae", icon: "🧫", ideogramName: "saccharomyces-cerevisiae" },
 };
 
 /**
